0ec5bd1de23ae74675aba5581d1e06bc7904b547
Faris Octa <m.faris.octa@gmail.com>
2023-03-20 13:34:06 +0700

    add handling errors on input

--- av_form.py
+++ av_form.py
@@ -2,13 +2,17 @@ import PySimpleGUI as sg
 
 # Data awal untuk tabel
 data = [
-    [0.4028, 6.984, 1.0045, 1, 0],
-    [1.0765, 4.73, 1.0045, 1, 0],
-    [1.211, 3.279, 1.0045, 1, 0]
+    [70, 0.4028, 6.984, 1.0045, 1, 0, 'Lakukan pemanasan'],
+    [240, 1.0765, 4.73, 1.0045, 1, 0, 'Tambah waktu mixing'],
+    [240, 1.211, 3.279, 1.0045, 1, 0, 'Tambah waktu mixing']
 ]
 
 # Header untuk tabel
-header = ['Berat Sample', 'Jumlah Titran', 'Faktor Buret', 'Faktor NaOH', 'AV']
+header = ['Suhu (\xb0C)', 'Berat Sample (gr)', 'Jumlah Titran (mL)', 'Faktor Buret', 'Faktor NaOH', 'AV', 'Instruksi']
+
+# AV Standard
+bottom_std = 20.5
+top_std = 22.0
 
 ### improvement feature ###
 # df = pd.read_csv('C:/Data/dummydata.csv')
@@ -17,15 +21,16 @@ header = ['Berat Sample', 'Jumlah Titran', 'Faktor Buret', 'Faktor NaOH', 'AV']
 
 # Layout GUI
 header_column = [
-    [sg.Text('Acid Value Form', font=('Arial', 20), justification='center')],
+    [sg.Text('S 1200-IK Acid Value Form', font=('Arial', 20), justification='center')],
     [sg.Text('INKALI QC', font=('Arial', 15), justification='center')]
 ]
 
-sample_identity_column = sg.Column([[sg.Text("identitas")]], justification='right')
-
 input_column = sg.Column([
-    [sg.Frame('Input Sample:',[[sg.Text('Berat Sample:', size=10), sg.InputText(key='-BERAT-SAMPLE-', size=(20,1))],
-                               [sg.Text('Jumlah Titran:', size=10), sg.InputText(key='-JUMLAH-TITRAN-', size=(20,1))],
+    [sg.Text('Lot:', size=10), sg.InputText(key='-LOT-', size=(20,1))],
+    [sg.Text('Operator QC:', size=10), sg.InputText(key='-OPERATOR-', size=(20,1))],
+    [sg.Frame('Input Sample:',[[sg.Text('Suhu (\xb0C):', size=10), sg.InputText(key='-SUHU-', size=(20,1))],
+                               [sg.Text('Berat Sample (gr):', size=10), sg.InputText(key='-BERAT-SAMPLE-', size=(20,1))],
+                               [sg.Text('Jumlah Titran (mL):', size=10), sg.InputText(key='-JUMLAH-TITRAN-', size=(20,1))],
                                [sg.Text('Faktor Buret:', size=10), sg.InputText(key='-FAKTOR-BURET-', size=(20,1))],
                                [sg.Text('Faktor NaOH:', size=10), sg.InputText(key='-FAKTOR-NaOH-', size=(20,1))]
                                ], pad=(0,0))],
@@ -33,9 +38,10 @@ input_column = sg.Column([
 ], pad=(0,0))
 
 table_column = [
-    [sg.Table(values=data, headings=header, def_col_width=12 , max_col_width=25,
+    [sg.Table(values=data, headings=header, col_widths=[8,14,14,12,12,10,20] , max_col_width=25,
               auto_size_columns=False, justification='center',
-              num_rows=7, display_row_numbers=True, background_color='black', key='-TABLE-')]  #num_rows=min(25, len(data))
+              num_rows=7, display_row_numbers=True, background_color='black', key='-TABLE-',)],   #num_rows=min(25, len(data))
+    [sg.Submit(pad=(300, 5))]
 ]
 
 
@@ -47,29 +53,52 @@ layout = [
         sg.VSeperator(),
         sg.Column(table_column, vertical_alignment='Top'),
     ],
-    [sg.Sizegrip()]
+    # [sg.Sizegrip()]
 ]
 
 # Membuat Window
-window = sg.Window('Acid Value Form', layout, keep_on_top=True, finalize=True, resizable=True)
+sg.theme('DarkGreen7')
+window = sg.Window('Acid Value Form', layout, keep_on_top=False, finalize=True, resizable=True)
 window.set_min_size(window.size)
 
 # Loop event
 while True:
     event, values = window.read()
-    if event in (None, 'Exit'):
+    if event in (sg.WIN_CLOSED, None):
         break
     if event == 'Add':
         # Ambil input dari user
-        berat_sample = round(float(values['-BERAT-SAMPLE-']),2)
-        jumlah_titran = float(values['-JUMLAH-TITRAN-'])
-        faktor_buret = float(values['-FAKTOR-BURET-'])
-        faktor_NaOH = float(values['-FAKTOR-NaOH-'])
-        AV = (jumlah_titran * faktor_buret * faktor_buret * faktor_NaOH * 5.61) / berat_sample
-        # Tambahkan baris baru ke tabel
-        data.append([berat_sample, jumlah_titran, faktor_buret, faktor_NaOH, AV])
-        # Update tampilan tabel
-        window['-TABLE-'].update(values=data)
+        try:
+            suhu = float(values['-SUHU-'])
+            berat_sample = round(float(values['-BERAT-SAMPLE-']),2)
+            jumlah_titran = float(values['-JUMLAH-TITRAN-'])
+            faktor_buret = float(values['-FAKTOR-BURET-'])
+            faktor_NaOH = float(values['-FAKTOR-NaOH-'])
+            AV = round((jumlah_titran * faktor_buret * faktor_buret * faktor_NaOH * 5.61) / berat_sample, 4)
+
+        # Logika
+            if suhu < 100:
+                instruksi = 'Lakukan pemanasan'
+                data.append([suhu, berat_sample, jumlah_titran, faktor_buret, faktor_NaOH, AV, instruksi])
+
+            elif 100 <= suhu < 200:
+                if AV < bottom_std:
+                    instruksi = 'Tambah Oleic Acid'
+                elif  bottom_std <= AV <= top_std:
+                    instruksi = 'Packing'
+                else:
+                    instruksi = 'Hubungi atasan'
+                data.append([suhu, berat_sample, jumlah_titran, faktor_buret, faktor_NaOH, AV, instruksi])
+
+            elif 200 <= suhu < 300:
+                if AV > top_std:
+                    instruksi = 'Tambah waktu pemanasan'
+                else:
+                    instruksi = 'Lakukan cooling'
+                data.append([suhu, berat_sample, jumlah_titran, faktor_buret, faktor_NaOH, AV, instruksi])
+            window['-TABLE-'].update(values=data)
+        except ValueError as e:
+            sg.PopupError(str('Mohon input data dengan benar'))
 
 # Keluar
 window.close()
--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import PySimpleGUI as sg
 
-# Set theme
-sg.theme('DarkBlue3')
+layout = [[sg.Text('Masukkan bilangan positif:'), sg.InputText()],
+          [sg.Submit(), sg.Cancel()]]
 
-# Define layout
-layout = [[sg.Column([[sg.Text('A:', justification='left')], 
-                      [sg.Text('Text 1', justification='left')]], element_justification='left' ), 
-           sg.Column([[sg.Text('B:', justification='right')], 
-                      [sg.Text('Text 2', justification='right')]], element_justification='right', grab=True)]]
+window = sg.Window('Contoh Input Bilangan Positif', layout)
 
-# Create window
-window = sg.Window('Two Columns with Different Justifications', layout, resizable=True, size=(400, 100))
-
-# Event loop
 while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    event, values = window.Read()
+    if event in (sg.WIN_CLOSED, 'Cancel'):
         break
+    try:
+        number = int(values[0])
+        if number <= 0:
+            raise ValueError('Anda harus memasukkan bilangan positif')
+        sg.Popup('Anda memasukkan bilangan positif: ' + str(number))
+    except ValueError as e:
+        sg.PopupError(str(e))
 
-# Close window
-window.close()
+window.Close()
+
+
 
